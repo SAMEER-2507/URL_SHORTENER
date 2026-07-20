41654bbfea8ec96b7e1ed118323bd829e70fc407
SAMEER-2507 <sv0456684@gmail.com>
2026-07-20 17:57:58 +0530

    Refactor server port configuration and logging

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -32,10 +32,10 @@ app.use("/api/create",short_url)
 app.get("/:id",redirectFromShortUrl)
 
 app.use(errorHandler)
-
-app.listen(3000,()=>{
+const port=process.env.PORT || 3000;
+app.listen(port,()=>{
     connectDB()
-    console.log("Server is running on http://localhost:3000");
+    //console.log("Server is running on http://localhost:3000");
 })
 
 // GET - Redirection 
